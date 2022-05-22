4187d06a0740ff47f97c67de71e7da01176430ab
Alejandro Montes <ango19941994@gmail.com>
2022-05-22 20:39:25 +0200

    Add functionality to app.js and tests

--- app.js
+++ app.js
@@ -1,176 +1,76 @@
-const prebioticBroth = [
-    [1, 1, 0, 1, 1],
-    [1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 1],
-    [1, 1, 0, 1, 1],
-];
-const prebioticBrothCopy = [
-    [1, 1, 0, 1, 1],
-    [1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 1],
-    [1, 1, 0, 1, 1],
-];
+const mapSize = 10;
 
-let neighbours = 0;
+let prebioticBroth = generateMap();
+const prebioticBrothCopy = prebioticBroth.map(function (arr) {
+    return arr.slice();
+});
 
-function checkCenter(arr, i, j) {
-    if (i === 0 && j === 0) {
-        if (arr[i][j + 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i + 1][j + 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i + 1][j] === 1) {
-            neighbours++;
-        }
-    } else if (i === 0 && j < arr.length && j !== 0) {
-        if (arr[i][j - 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i][j + 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i + 1][j] === 1) {
-            neighbours++;
-        }
-        if (arr[i + 1][j - 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i + 1][j + 1] === 1) {
-            neighbours++;
-        }
-    } else if (i === 0 && j === arr[i].length) {
-        if (arr[i][j - 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i + 1][j] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j - 1] === 1) {
-            neighbours++;
-        }
-    } else if (i === arr.length - 1 && j === 0) {
-        if (arr[i][j + 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j + 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j] === 1) {
-            neighbours++;
-        }
-    } else if (i === arr.length - 1 && j < arr[i].length && j !== 0) {
-        if (arr[i][j - 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i][j + 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j - 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j + 1] === 1) {
-            neighbours++;
-        }
-    } else if (i === arr.length && j === arr[i].length) {
-        if (arr[i][j - 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j - 1] === 1) {
-            neighbours++;
-        }
-    } else {
-        if (arr[i][j - 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i][j + 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j] === 1) {
-            neighbours++;
-        }
-        if (arr[i + 1][j] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j - 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i + 1][j + 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i - 1][j + 1] === 1) {
-            neighbours++;
-        }
-        if (arr[i + 1][j - 1] === 1) {
-            neighbours++;
-        }
+function generateMap() {
+    const generatedArr = [];
+    for (let i = 0; i < mapSize; i++) {
+        generatedArr.push(
+            Array.from({ length: mapSize }, () => Math.round(Math.random()))
+        );
     }
+    return generatedArr;
 }
-function changePrebioticBroth(arrCopy, i, j) {
+export function checkNeighbours(arr, i, j) {
+    let neighbours = 0;
+    if (typeof arr[i][j - 1] !== 'undefined' && arr[i][j - 1] === 1) {
+        neighbours++;
+    }
+    if (typeof arr[i][j + 1] !== 'undefined' && arr[i][j + 1] === 1) {
+        neighbours++;
+    }
+    if (typeof arr[i - 1] !== 'undefined' && arr[i - 1][j] === 1) {
+        neighbours++;
+    }
+    if (typeof arr[i + 1] !== 'undefined' && arr[i + 1][j] === 1) {
+        neighbours++;
+    }
+    if (typeof arr[i - 1] !== 'undefined' && arr[i - 1][j - 1] === 1) {
+        neighbours++;
+    }
+    if (typeof arr[i + 1] !== 'undefined' && arr[i + 1][j + 1] === 1) {
+        neighbours++;
+    }
+    if (typeof arr[i - 1] !== 'undefined' && arr[i - 1][j + 1] === 1) {
+        neighbours++;
+    }
+    if (typeof arr[i + 1] !== 'undefined' && arr[i + 1][j - 1] === 1) {
+        neighbours++;
+    }
+    // }
+    return neighbours;
+}
+function changeMap(arr, arrCopy, i, j) {
+    const neighbours = checkNeighbours(arr, i, j);
     if (neighbours < 2) {
         arrCopy[i][j] = 0;
     }
     if (neighbours > 3) {
         arrCopy[i][j] = 0;
     }
-    if (neighbours === 2 && neighbours === 3) {
-        arrCopy[i][j] = 1;
-    }
     if (neighbours === 3) {
         arrCopy[i][j] = 1;
     }
 }
-function countNeighbours(arr, arrCopy) {
+function runExperiment(arr, arrCopy) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
-            checkCenter(arr, i, j);
-            changePrebioticBroth(arrCopy, i, j);
-            neighbours = 0;
+            changeMap(arr, arrCopy, i, j);
         }
     }
-    console.log(neighbours);
+
+    prebioticBroth = prebioticBrothCopy.map(function (arr) {
+        return arr.slice();
+    });
+
+    console.log(prebioticBroth);
 }
 
 console.log(prebioticBroth);
-countNeighbours(prebioticBroth, prebioticBrothCopy);
-console.log(prebioticBrothCopy);
 
-// function panel(arr, arrCopy) {
-//     debugger;
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = 0; j < arr.length; j++) {
-//             if (
-//                 arr[i][j] === 1 &&
-//                 arr[i][j - 1] === 1 &&
-//                 arr[i][j + 1] === 1 &&
-//                 j !== 0
-//             ) {
-//                 arrCopy[i][j - 1] = 0;
-//                 arrCopy[i][j + 1] = 0;
-//                 arrCopy[i - 1][j] = 1;
-//                 arrCopy[i + 1][j] = 1;
-//             }
-//             if (
-//                 i !== 0 &&
-//                 i !== arr.length + 1 &&
-//                 arr[i][j] === 1 &&
-//                 arr[i + 1][j] === 1 &&
-//                 arr[i - 1][j] === 1
-//             ) {
-//                 arrCopy[i][j - 1] = 1;
-//                 arrCopy[i][j + 1] = 1;
-//                 arrCopy[i - 1][j] = 0;
-//                 arrCopy[i + 1][j] = 0;
-//             }
-//         }
-//     }
-// }
+setInterval(function () {
+    runExperiment(prebioticBroth, prebioticBrothCopy);
+}, 3000);
